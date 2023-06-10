772e328dbed44227f9f0653ea890b1a07003ec41
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-10 11:16:34 +0330

    install composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4a65c0ed5f3ab3b7ca3c12249ad9dd8faa32358d',
+        'name' => 'emo/emo-logo-adder',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'emo/emo-logo-adder' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4a65c0ed5f3ab3b7ca3c12249ad9dd8faa32358d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
