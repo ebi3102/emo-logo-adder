a19e6ca818da8bcc7d270396752ef877db95088d
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-10 11:09:13 +0330

    create main wordpress plugin php file

---
+++ emo-logo-adder.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package EMO_LA
+ * Plugin Name: Emo Logo Adder for WooCommerce
+ * Plugin URI:
+ * Description: A powerful system adding logo and othe images to product image to customize product for customer
+ * Author: Ebrahim Moeini
+ * Author URI: https://emoeini.com
+ * Version: 1.0.0
+ * Requires at least: 5.8
+ * Tested up to: 6.2
+ * WC requires at least: 5.5.0
+ * WC tested up to: 7.5.1
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: emo-logo-adder-for-woocommerce
+ * Domain Path: /languages
+ **/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
