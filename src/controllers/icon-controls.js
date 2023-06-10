1b6638efe5d48000a67989017f419ab4208cc403
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-10 08:33:00 +0330

    complete skew actions

--- src/controllers/icon-controls.js
+++ src/controllers/icon-controls.js
@@ -2,51 +2,83 @@ import {skewYController, skewXController} from "./skew-controller";
 
 export {customiseControls, customiseCornerIcons}
 
+const imgCursor = encodeURIComponent(`
+  <svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='24' height='24'>
+    <defs>
+      <filter id='a' width='266.7%' height='156.2%' x='-75%' y='-21.9%' filterUnits='objectBoundingBox'>
+        <feOffset dy='1' in='SourceAlpha' result='shadowOffsetOuter1'/>
+        <feGaussianBlur in='shadowOffsetOuter1' result='shadowBlurOuter1' stdDeviation='1'/>
+        <feColorMatrix in='shadowBlurOuter1' result='shadowMatrixOuter1' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0'/>
+        <feMerge>
+          <feMergeNode in='shadowMatrixOuter1'/>
+          <feMergeNode in='SourceGraphic'/>
+        </feMerge>
+      </filter>
+      <path id='b' d='M1.67 12.67a7.7 7.7 0 0 0 0-9.34L0 5V0h5L3.24 1.76a9.9 9.9 0 0 1 0 12.48L5 16H0v-5l1.67 1.67z'/>
+    </defs>
+    <g fill='none' fill-rule='evenodd'><path d='M0 24V0h24v24z'/>
+      <g fill-rule='nonzero' filter='url(#a)' transform='rotate(-90 9.25 5.25)'>
+        <use fill='#000' fill-rule='evenodd' xlink:href='#b'/>
+        <path stroke='#FFF' d='M1.6 11.9a7.21 7.21 0 0 0 0-7.8L-.5 6.2V-.5h6.7L3.9 1.8a10.4 10.4 0 0 1 0 12.4l2.3 2.3H-.5V9.8l2.1 2.1z'/>
+      </g>
+    </g>
+  </svg>`);
+
 const customiseControls = {
     tl: {
-        action: 'rotate',
-        cursor: 'pointer'
+        action: 'scale',
+        cursor: 'nw-resize'
     },
     tr: {
         action: 'scale',
-        cursor: 'pointer'
+        cursor: 'ne-resize'
     },
     bl: {
-        action: 'remove',
-        cursor: 'pointer'
+        action: 'scale',
+        cursor: 'nw-resize'
     },
     br: {
-        action: 'skew',
-        cursor: 'pointer'
+        action: 'scale',
+        cursor: 'ne-resize'
     },
     mb: {
         action: function( event, target ) {
-            target.canvas.on('mouse:down', (options)=> skewXController(options, target))
+            target.canvas.on('mouse:down', (options)=> skewXController(options, target, 'mb'))
         },
-        cursor: 'pointer'
+        cursor: 'e-resize'
     },
-    mt: false,
+    mt: {
+        action: function( event, target ) {
+            target.canvas.on('mouse:down', (options)=> skewXController(options, target, 'mt'))
+        },
+        cursor: 'e-resize'
+    },
     mr: {
         action: function( event, target ) {
-            target.canvas.on('mouse:down', (options)=> skewYController(options, target))
+            target.canvas.on('mouse:down', (options)=> skewYController(options, target, 'mr'))
         },
-        cursor: 'crosshair'
+        cursor: 'n-resize'
     },
-    ml:false,
+    ml:{
+        action: function( event, target ) {
+            target.canvas.on('mouse:down', (options)=> skewYController(options, target, 'ml'))
+        },
+        cursor: 'n-resize'
+    },
     // only is hasRotatingPoint is not set to false
     mtr: {
         action: 'rotate',
-        cursor: 'pointer'
+        cursor: `url("data:image/svg+xml;charset=utf-8,${imgCursor}") 12 12, crosshair`
     },
 };
 
 const customiseCornerIcons ={
     settings: {
         borderColor: 'black',
-        cornerSize: 10,
+        cornerSize: 18,
         cornerShape: 'rect',
-        cornerBackgroundColor: '#fff',
-        cornerPadding: 2,
+        cornerBackgroundColor: 'transparent',
+        cornerPadding: 4,
     },
     tl: {
         icon: 'icons/rotate.svg',
--- src/controllers/skew-controller.js
+++ src/controllers/skew-controller.js
@@ -1,18 +1,22 @@
 export {skewYController, skewXController}
-function skewYController(options, activeObject){
-    if(options.target && options.transform.corner == 'mr'){
+function skewYController(options, activeObject, cornerName){
+    if(options.target && options.transform.corner == cornerName){
         activeObject.lockMovementX = true; 
         activeObject.lockMovementY = true;
-        let controller = true        
-        const startPoint = {X:options.e.layerX, Y:options.e.layerY }
+        let controller = true;
+        let startPoint = {X:options.e.layerX, Y:options.e.layerY }
         activeObject.canvas.on('mouse:move', function(options1) {
-            if(options1.target && options1.target.__corner == 'mr' && controller){
+            if(options1.target && options1.target.__corner == cornerName && controller){
                 activeObject.lockMovementX = false; 
                 activeObject.lockMovementY = false;
-                // const deltaX = startPoint.X - options1.pointer.x;
                 const deltaY = startPoint.Y - options1.pointer.y;
-                // activeObject.skewX =activeObject.skewX+ deltaX;
-                activeObject.skewY =activeObject.skewY+ deltaY;
+                startPoint = {X:options1.pointer.x, Y:options1.pointer.y}
+                if(cornerName == 'mr'){
+                    activeObject.skewY =activeObject.skewY - deltaY;
+                }else if(cornerName == 'ml'){
+                    activeObject.skewY =activeObject.skewY + deltaY;
+                }
+                
             }
         })
         activeObject.canvas.on('mouse:up', (options3)=>{
@@ -23,20 +27,19 @@ function skewYController(options, activeObject){
     }
 }
 
-function skewXController(options, activeObject){
-    if(options.target && options.transform.corner == 'mb'){
+function skewXController(options, activeObject, cornerName){
+    if(options.target && options.transform.corner == cornerName){
         activeObject.lockMovementX = true; 
         activeObject.lockMovementY = true;
         let controller = true        
-        const startPoint = {X:options.e.layerX, Y:options.e.layerY }
+        let startPoint = {X:options.e.layerX, Y:options.e.layerY }
         activeObject.canvas.on('mouse:move', function(options1) {
-            if(options1.target && options1.target.__corner == 'mb' && controller){
+            if(options1.target && options1.target.__corner == cornerName && controller){
                 activeObject.lockMovementX = false; 
                 activeObject.lockMovementY = false;
                 const deltaX = startPoint.X - options1.pointer.x;
-                // const deltaY = startPoint.Y - options1.pointer.y;
+                startPoint = {X:options1.pointer.x, Y:options1.pointer.y}
                 activeObject.skewX =activeObject.skewX+ deltaX;
-                // activeObject.skewY =activeObject.skewY+ deltaY;
             }
         })
         activeObject.canvas.on('mouse:up', (options3)=>{
