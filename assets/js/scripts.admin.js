aefb9d8208b9255de7aef654847816206cc34879
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-16 07:16:34 +0330

    Add comments on this file

--- assets/js/scripts.admin.js
+++ assets/js/scripts.admin.js
@@ -1,13 +1,14 @@
 jQuery('document').ready(function($){
+    // Show Edtior Modal
     $('#addEditorPopUp').click(function(e){
         e.preventDefault();
         $('.popup-screen-locker').css('display', 'block');
         $('.popup-editor-container').css('display', 'block');
     })
 
+    // Close editor Modal
     $('#emoClose').click((e)=>{
         $('.popup-screen-locker').css('display', 'none');
         $('.popup-editor-container').css('display', 'none');
     })
-
 });
