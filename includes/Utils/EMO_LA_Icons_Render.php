52a50c7b1a0eb06a654d98dd3bca01418a1340a2
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-16 15:12:43 +0330

    Add defualt logo data

--- includes/Utils/EMO_LA_Icons_Render.php
+++ includes/Utils/EMO_LA_Icons_Render.php
@@ -28,11 +28,16 @@ class EMO_LA_Icons_Render
     public function imageRender()
     {
         $logoData = get_post_meta( $this->postObject->ID, EMO_LA_LOGO_DATA, true );
-        
+        $defualtLogoData = array(
+            "left" => 150,
+            "top" => 200,
+            "scaleX" => 0.4,
+            "scaleY" => 0.4
+        );
         return array(
             "background" => get_the_post_thumbnail_url($this->postObject),
-            "logo" =>($logoData)? json_decode($logoData)->src : EMO_LA_URI."assets/images/logo.png",
-            "logoData" => ($logoData)? $logoData:"false"
+            "logo" =>(!$logoData || $logoData == 'undefined')? EMO_LA_URI."assets/images/logo.png" : json_decode($logoData)->src,
+            "logoData" => (!$logoData || $logoData == 'undefined')? json_encode($defualtLogoData) : $logoData
         );
     }
 
