3f8cbedf670293e0d2e82868ae97f311565de471
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-24 18:34:45 +0330

    Define admin page settings fields

--- includes/EMO_LA_Admin_Settings.php
+++ includes/EMO_LA_Admin_Settings.php
@@ -6,11 +6,12 @@ class EMO_LA_Admin_Settings
     public function __construct()
     {
         add_action( 'admin_menu', array($this, 'emo_la_add_admin_page') );
+        add_action('admin_init' , array($this,'emo_la_custom_settings'));
     }
 
     public function emo_la_add_admin_page()
     {
-        add_menu_page( __( 'Logo Editor Settings', 'emo_la' ), __( 'Logo Editor', 'emo_la' ), 'manage_options', '', array($this, 'emo_la_create_page'), 'dashicons-image-rotate-left', 110 );
+        add_menu_page( __( 'Logo Editor Settings', 'emo_la' ), __( 'Logo Editor', 'emo_la' ), 'manage_options', 'emo_la_slug', array($this, 'emo_la_create_page'), 'dashicons-image-rotate-left', 110 );
         //Generate Sunset Admin Sub Pages
         add_submenu_page('emo_la_slug', __( 'Logo Editor Settings', 'emo_la' ), __( 'Settings', 'emo_la' ) , 'manage_options' , 'emo_la_slug' , array($this, 'emo_la_create_page'));
 
@@ -19,5 +20,28 @@ class EMO_LA_Admin_Settings
     public function emo_la_create_page()
     {
 	    require_once 'templates/admin-settings.php';
+    }
+
+    public function emo_la_custom_settings()
+    {
+        add_settings_section('emo_la_printed_settings', __( 'Set printed product', 'emo_la' ), array($this,'emo_la_printed_settings_callback'), 'emo_la_slug');
+
+        register_setting('emo_la_product_settings', 'monochrome_print');
+	    add_settings_field('emo_la_monochrome_print', __( 'Set the monochrome product ', 'emo_la' ), array($this,'emo_la_monochrome_print_callback'), 'emo_la_slug', 'emo_la_printed_settings');
+
+
+        register_setting('emo_la_product_settings', 'bichrome_print');
+        add_settings_field('emo_la_bichrome_print', __( 'Set the bichrome product ', 'emo_la' ), array($this,'emo_la_bichrome_print_callback'), 'emo_la_slug', 'emo_la_printed_settings');
+        
+    }
+
+    public function emo_la_printed_settings_callback(){}
+
+    public function emo_la_monochrome_print_callback(){
+        echo "monochrome product";
+    }
+
+    public function emo_la_bichrome_print_callback(){
+        echo "bichrome product";
     }
 }
--- includes/templates/admin-settings.php
+++ includes/templates/admin-settings.php
@@ -1,6 +1,6 @@
 <h1><?php echo __( 'Logo Editor Settings', 'emo_la' ) ?></h1>
 <form action="options.php" method="post" class="sunset-general-form">
-    <?php //settings_fields('emo_ecb_settings_group'); ?>
-    <?php// do_settings_sections('emo_ecb_slug'); ?>
-    <?php //submit_button( __('Save stettings', 'emo_la'), 'primary', 'btnSubmit'); ?>
+    <?php settings_fields('emo_la_product_settings'); ?>
+    <?php do_settings_sections('emo_la_slug'); ?>
+    <?php submit_button( __('Save settings', 'emo_la'), 'primary', 'btnSubmit'); ?>
 </form>
