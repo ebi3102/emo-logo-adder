2de2de67348539b0e58df7798cef445ec4f4efae
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-27 05:35:12 +0330

    Set activation for logo editor

--- includes/EMO_LA_Client_Editor_Icon.php
+++ includes/EMO_LA_Client_Editor_Icon.php
@@ -20,6 +20,8 @@ class EMO_LA_Client_Editor_Icon
     public function editor_template()
     { 
         global $post;
+        if(!get_post_meta( $this->postObject->ID, EMO_LA_LOGO_DATA, true ))
+            return;
         $uploadedLogoData = new EMO_LA_Inject_JS_Client($post, 'client-scripts');
         $uploadedLogoData->render();
         //Add Logo and Icons detailes
