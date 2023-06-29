384dda356d3e7e854562665ed2d74f4e1749ece3
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-29 09:08:28 +0330

    Create save_item_local to add one item to the local strorage

--- js-develop/controllers/client-save-localStorage.js
+++ js-develop/controllers/client-save-localStorage.js
@@ -1,4 +1,4 @@
-export {save_to_local}
+export {save_to_local, save_item_local}
 function save_to_local(saveData, logoData ){
     const image = canvas.toDataURL({
         format: 'png',
@@ -38,4 +38,32 @@ function save_to_local(saveData, logoData ){
         localStorage.removeItem("emoEditorData");
     }
     localStorage.setItem('emoEditorData',  JSON.stringify(storageData));
+}
+
+/**
+ * This function add just one item to local storage
+ * It is useful for adding printed product or any other things to local storage
+ * @param {*} storageName 
+ * @param {*} postID 
+ * @param {*} key 
+ * @param {*} value 
+ */
+function save_item_local(storageName, postID, key, value){
+
+    let storageData = localStorage.getItem(storageName);
+    if(!storageData){
+        storageData = {
+            [postID]: {key: value}
+        };
+    }else{
+        storageData = JSON.parse(storageData);
+        if(!storageData[postID]){
+            storageData[postID] = {key:value} ;
+        }else{
+            storageData[postID][key] = value;
+        }
+        localStorage.removeItem(storageName);
+    }
+    localStorage.setItem(storageName, JSON.stringify(storageData));
+
 }
