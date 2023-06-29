0768def05bd124da304d7f318065a0c5ec4d1a76
ebi3102 <ebi3102@gmail.com>
2023-06-29 23:41:25 +0330

    Remove unused Btn

--- includes/EMO_LA_Editor_MetaBox.php
+++ includes/EMO_LA_Editor_MetaBox.php
@@ -25,46 +25,39 @@ class EMO_LA_Editor_MetaBox
 
     public function metabox_callback($post)
     {
-        $btnText = __("Add logo editor setting", 'emo_logo_adder');
-        if(has_post_thumbnail($post)){
-            global $canvasData;
-            echo "<button id='addEditorPopUp'>{$btnText}</button>";
-            echo "<div class='thumbnails-container'>";
-            $this->thumbnails_template($post);
-            echo "</div>";
-            ?>
-            <div id="popupScreenLocker" class="popup-screen-locker">
-                <div id="popupEditorContainer" class="popup-editor-container" style="width:<?php echo $canvasData['width'].'px' ?>">
-                    <div>
-                        <div id="emoClose" class="close-icon">&#10005;</div>
-                        <?php 
-                        //Add Logo and Icons detailes
-                        $canvasDate = new EMO_LA_Icons_Render($post, 'emo-image-editor');
-                        echo $canvasDate->render();
-                        ?>
-                        <div id="canvasContainer"></div>
-                        <div class="btn-container">
-                            <div id="emoUploadlogo" class="emo-btn primary">Upload new logo</div>
-                            <div
-                                id="emoSaveEditor" 
-                                class="emo-btn success" 
-                                data-nonce="<?php echo wp_create_nonce("emo_la_nonce".$post->ID); ?>"
-                                data_id = "<?php echo $post->ID ?>"
-                                data_url = "<?php echo admin_url( 'admin-ajax.php' ) ?>"
-                            >Save</div>
-                        </div>
-                        <div id="loadingImg" class="loading-img"><img src="<?php echo EMO_LA_URI.'assets/images/loading.gif'?>" alt=""></div>
-                        <div class="notice-container" id="noticeContainer"></div>
-                    </div>
-                </div>
-            </div><!--.popup-screen-locker-->
 
-        <?php 
-        }else{
-            $commentText = __('Before adding any editor settings set a feature image for this product', 'emo_logo_adder');
-            echo "<button id='addEditorPopUp' disabled>{$btnText}</button>";
-            echo "<div>{$commentText}</div>";
-        }
+        global $canvasData;
+        echo "<div class='thumbnails-container'>";
+        $this->thumbnails_template($post);
+        echo "</div>";
+        ?>
+        <div id="popupScreenLocker" class="popup-screen-locker">
+            <div id="popupEditorContainer" class="popup-editor-container" style="width:<?php echo $canvasData['width'].'px' ?>">
+                <div>
+                    <div id="emoClose" class="close-icon">&#10005;</div>
+                    <?php
+                    //Add Logo and Icons detailes
+                    $canvasDate = new EMO_LA_Icons_Render($post, 'emo-image-editor');
+                    echo $canvasDate->render();
+                    ?>
+                    <div id="canvasContainer"></div>
+                    <div class="btn-container">
+                        <div id="emoUploadlogo" class="emo-btn primary">Upload new logo</div>
+                        <div
+                            id="emoSaveEditor"
+                            class="emo-btn success"
+                            data-nonce="<?php echo wp_create_nonce("emo_la_nonce".$post->ID); ?>"
+                            data_id = "<?php echo $post->ID ?>"
+                            data_url = "<?php echo admin_url( 'admin-ajax.php' ) ?>"
+                        >Save</div>
+                    </div>
+                    <div id="loadingImg" class="loading-img"><img src="<?php echo EMO_LA_URI.'assets/images/loading.gif'?>" alt=""></div>
+                    <div class="notice-container" id="noticeContainer"></div>
+                </div>
+            </div>
+        </div><!--.popup-screen-locker-->
+
+        <?php
     }
 
     private function thumbnails_template($post)
