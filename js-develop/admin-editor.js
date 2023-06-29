0cf41fdb8ee4d491d34932d4e97a4df402460b17
ebi3102 <ebi3102@gmail.com>
2023-06-29 19:40:13 +0330

    create click event to show canvas on admin imgsbtn

--- js-develop/admin-editor.js
+++ js-develop/admin-editor.js
@@ -6,7 +6,7 @@ import { adminFabric } from "./editors/admin-fabric";
 let imgsBtn = document.getElementsByClassName('thumbnail-editor');
 for (let item of imgsBtn) {
     item.addEventListener('click', function(e){
-        console.log(this.src);
+        canvasContainer.innerHTML = '';
         adminFabric(this.src, this.getAttribute('data-id'));
         popupScreenLocker.style.display = 'block';
     });
