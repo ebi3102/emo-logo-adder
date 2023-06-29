bddc285e144702d11a4202df7759ae7107d6c971
Ebrahim Moeini <ebi3102@gmail.com>
2023-06-29 16:18:25 +0330

    Modify to hook the emoEditorData to oreder

--- assets/js/scripts.order.js
+++ assets/js/scripts.order.js
@@ -1,10 +1,19 @@
-document.querySelector('form[name=checkout]').addEventListener('submit', function(event){
-    // event.preventDefault();
-    var localData = localStorage.getItem('emoEditorData');
-    var hiddenInput = document.createElement('input');
-    hiddenInput.type = 'hidden';
-    hiddenInput.name = 'emoEditorData';
-    hiddenInput.value = localData;
-    this.appendChild(hiddenInput);
+// document.querySelector('form[name=checkout]').addEventListener('submit', function(event){
+//     event.preventDefault();
+//     var localData = localStorage.getItem('emoEditorData');
+//     var hiddenInput = document.createElement('input');
+//     hiddenInput.type = 'hidden';
+//     hiddenInput.name = 'emoEditorData';
+//     hiddenInput.value = localData;
+//     this.appendChild(hiddenInput);
     
-});
+// });
+
+var formCheckout = document.querySelector('form[name=checkout]');
+var localData = localStorage.getItem('emoEditorData');
+var hiddenInput = document.createElement('input');
+hiddenInput.type = 'hidden';
+hiddenInput.name = 'emoEditorData';
+hiddenInput.value = localData;
+formCheckout.appendChild(hiddenInput);
+    
--- includes/Controllers/EMO_LA_Add_To_Order.php
+++ includes/Controllers/EMO_LA_Add_To_Order.php
@@ -8,7 +8,8 @@ class EMO_LA_Add_To_Order
 
     public function __construct()
     {
-        add_action('woocommerce_checkout_create_order', array($this, 'add_data_to_order'), 10, 2);
+        add_filter('woocommerce_checkout_create_order', array($this, 'emo_add_data_to_order'), 10, 1);
+        
     }
 
     private function field_setter()
@@ -16,11 +17,14 @@ class EMO_LA_Add_To_Order
         $this->emoEditorData = EMO_LA_Request_Handler::get_post( 'emoEditorData' );
     }
 
-    public function add_data_to_order($order, $data) {
+    public function emo_add_data_to_order($order) {
+        $this->field_setter();
         if (isset($this->emoEditorData)) {
             $myData = sanitize_text_field($this->emoEditorData);
             $order->update_meta_data(EMO_LA_ORDER_META, $myData);
+            $order->update_meta_data('test_order_data', json_encode($order->get_items()));
             $order->save();
         }
+        return $order;
     }
 }
