cff1a515c829bd20cb579cadbac78c09a6657a5e
ebi3102 <ebi3102@gmail.com>
2023-06-30 13:31:51 +0330

    add logo data and new image to order meta

--- assets/js/scripts.order.js
+++ assets/js/scripts.order.js
@@ -1,45 +1,51 @@
-// document.querySelector('form[name=checkout]').addEventListener('submit', function(event){
-//     event.preventDefault();
-//     var localData = localStorage.getItem('emoEditorData');
-//     var hiddenInput = document.createElement('input');
-//     hiddenInput.type = 'hidden';
-//     hiddenInput.name = 'emoEditorData';
-//     hiddenInput.value = localData;
-//     this.appendChild(hiddenInput);
+document.querySelector('form[name=checkout]').addEventListener('submit', function(event){
+    // event.preventDefault();
+    // var localData = localStorage.getItem('emoEditorData');
+    // var hiddenInput = document.createElement('input');
+    // hiddenInput.type = 'hidden';
+    // hiddenInput.name = 'emoEditorData';
+    // hiddenInput.value = localData;
+    // this.appendChild(hiddenInput);
+    localStorage.removeItem("emoEditorData");
     
-// });
+});
 
 var formCheckout = document.querySelector('form[name=checkout]');
 var localData = localStorage.getItem('emoEditorData');
 
-var hiddenInput = document.createElement('input');
-hiddenInput.type = 'hidden';
-hiddenInput.name = 'emoEditorData';
 
-var logosData = {};
-var images = []
-objLocalData = JSON.parse(localData);
-for (const postID in objLocalData) {
-    logosData[postID] = {};
-    for(imageID in objLocalData[postID]){
-        logosData[postID][imageID] = objLocalData[postID][imageID].logoData;
-        images.push(objLocalData[postID][imageID].newImage)
-    }
-}
+// var logosData = {};
+// var images = []
+// objLocalData = JSON.parse(localData);
+// var formData = new FormData();
+// for (const postID in objLocalData) {
+//     logosData[postID] = {};
+//     for(imageID in objLocalData[postID]){
+//         logosData[postID][imageID] = objLocalData[postID][imageID].logoData;
+//         // images.push(objLocalData[postID][imageID].newImage)
+//         var imageData = objLocalData[postID][imageID].newImage;
+//         var binaryData = atob(imageData.split(',')[1]);
+//         var blob = new Blob([binaryData], { type: 'image/png' });
+//         formData.append('image', blob, 'image.png');
+//     }
+// }
 
 var logosElemInput = document.createElement('input');
 logosElemInput.name =  'logoData';
 logosElemInput.type = 'hidden';
-logosElemInput.value = logosData;
+logosElemInput.value =  localData;
 
-var imageElemInput = document.createElement('input');
-imageElemInput.name =  'imageData[]';
-imageElemInput.multiple = true;
-imageElemInput.type = 'hidden';
+// var imageElemInput = document.createElement('input');
+// imageElemInput.name =  'imageData';
+// imageElemInput.multiple = true;
+// imageElemInput.type = 'hidden';
+//
+// imageElemInput.value = JSON.stringify(formData);
 
-imageElemInput.value = images;
+
+console.log("input_Value: ", logosElemInput.value);
 
 formCheckout.appendChild(logosElemInput);
-formCheckout.appendChild(imageElemInput);
+// formCheckout.appendChild(imageElemInput);
 
 
--- includes/Controllers/EMO_LA_Add_To_Order.php
+++ includes/Controllers/EMO_LA_Add_To_Order.php
@@ -4,27 +4,51 @@ namespace EMO_LA\Controllers;
 use EMO_LA\Utils\EMO_LA_Request_Handler;
 class EMO_LA_Add_To_Order
 {
-    private $emoEditorData;
+    private $emoLogoData;
+    private $imagesData;
 
     public function __construct()
     {
         add_filter('woocommerce_checkout_create_order', array($this, 'emo_add_data_to_order'), 10, 1);
-
-        // $logoEditorData = json_decode( get_post_meta(70, EMO_LA_ORDER_META, true));
-        // $logoOrderData = json_decode(get_post_meta(70, "test_order_data", true));
     }
 
     private function field_setter()
     {
-        $this->emoEditorData = EMO_LA_Request_Handler::get_post( 'emoEditorData' );
+        $this->emoLogoData = EMO_LA_Request_Handler::get_post( 'logoData' );
     }
 
     public function emo_add_data_to_order($order) {
         $this->field_setter();
-        if (isset($this->emoEditorData)) {
-            $myData = sanitize_text_field($this->emoEditorData);
+        if (isset($this->emoLogoData)) {
+
+            $order_data = json_decode(stripslashes($this->emoLogoData),true );
+            $logosData = [];
+            $images = [];
+            foreach ($order_data as $postID=>$logosID){
+                $logosData[$postID] = [];
+                foreach ($logosID as $key=>$item){
+                    $logosData[$postID][$key] = $item['logoData'];
+                    $images[] = $item['newImage'];
+                }
+            }
+            $myData = sanitize_text_field(json_encode($logosData));
             $order->update_meta_data(EMO_LA_ORDER_META, $myData);
-            // $order->update_meta_data('test_order_data', json_encode($order->get_items()));
+
+            $targetDirectory = EMO_LA_CREATED_DIR. '/';
+            $imgUrls = [];
+            foreach ($images as $index=>$imgData){
+                $imageData = str_replace('data:image/png;base64,', '', $imgData);
+                $imageData = str_replace(' ', '+', $imageData);
+                $imageData = base64_decode($imageData);
+                $targetFilename = $targetDirectory . 'image_' . $index .'_'.time(). '.png';
+                $targetFileUrl = EMO_LA_CREATED_URI . 'image_' . $index .'_'.time(). '.png';
+                // Save the image data to the target file
+                file_put_contents($targetFilename, $imageData);
+                $imgUrls[] = $targetFileUrl;
+            }
+            $urlsData = sanitize_text_field(json_encode($imgUrls));
+            $order->update_meta_data('emo_editor_logo_order', $urlsData);
+
             $order->save();
         }
         return $order;
