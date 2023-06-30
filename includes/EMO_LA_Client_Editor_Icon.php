ba16eb8813c87cfa81832e78ac9ed61ed13badc9
ebi3102 <ebi3102@gmail.com>
2023-06-30 04:56:55 +0330

    Remove additional lines

--- includes/EMO_LA_Client_Editor_Icon.php
+++ includes/EMO_LA_Client_Editor_Icon.php
@@ -10,8 +10,6 @@ class EMO_LA_Client_Editor_Icon
 {
     public function __construct()
     {
-        // add_filter( 'woocommerce_single_product_image_thumbnail_html', array($this, 'editor_template'), 10, 2);
-        // add_action('woocommerce_before_single_product_summary', array($this, 'editor_template'), 30);
         add_action('woocommerce_product_thumbnails', array($this, 'editor_template'), 20);
         
         
